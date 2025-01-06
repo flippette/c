e1273d31d9f88ec74d02827d1f8beb865470ad23
flippette <me@flippo.cc>
2025-01-06 21:44:57 +0200

    t2.5

---
+++ t2.5/source.c
@@ -0,0 +1,7 @@
+#include <math.h>
+
+#include "source.h"
+
+double vectorlength(double x, double y, double z) {
+  return sqrt(x * x + y * y + z * z);
+}
---
+++ t2.5/source.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef SOURCE_H
+#define SOURCE_H
+
+double vectorlength(double x, double y, double z);
+
+#endif
